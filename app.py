56a7e981f0409f750def22fea239ed4b959cedef
Kim Hannah <soa6318@gmail.com>
2022-11-20 16:37:27 +0900

    Moved html pages to 'members' folder \nCreated 'index2.html' with my version of comments sorting logic \nChanged the API url name to '/api/comments' for both POST and GET requests

--- app.py
+++ app.py
@@ -5,38 +5,41 @@ import certifi
 ca = certifi.where()
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ckdals1994:ckdalsla94@cluster0.xtrzoeq.mongodb.net/Cluster0?retryWrites=true&w=majority',  tlsCAFile=ca)
-db = client.mini_project
+# client = MongoClient('mongodb+srv://ckdals1994:ckdalsla94@cluster0.xtrzoeq.mongodb.net/Cluster0?retryWrites=true&w=majority',  tlsCAFile=ca)
+# db = client.mini_project
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ecmqblf.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
 @app.route('/')
 def index_1():
-  return render_template('index.html')
+  return render_template('index2.html')
 
 @app.route('/red')
 def red():
-  return render_template('red.html')
+  return render_template('members/red.html')
 
 @app.route('/blue')
 def blue():
-  return render_template('blue.html')
+  return render_template('members/blue.html')
 
 @app.route('/black')
 def black():
-  return render_template('black.html')
+  return render_template('members/black.html')
 
 @app.route('/yellow')
 def yellow():
-  return render_template('yellow.html')
+  return render_template('members/yellow.html')
 
 @app.route('/pink')
 def pink():
-  return render_template('pink.html')
+  return render_template('members/pink.html')
 
 @app.route('/about_jn')
 def about_jn():
-  return render_template('about_jn.html')
+  return render_template('members/about_jn.html')
 
-@app.route("/api/save_comment", methods=["POST"])
+# POST
+@app.route("/api/comments", methods=["POST"])
 def homework_post():
   name_receive = request.form['name_give']
   comment_receive = request.form['comment_give']
@@ -46,13 +49,16 @@ def homework_post():
     'comment': comment_receive,
     'time': time_receive
   }
-  db.guest.insert_one(doc)
-  return jsonify({'msg':'댓글 입력 완료!'})
+  # db.guest.insert_one(doc)
+  db.miniProject.insert_one(doc)
+  return jsonify({'msg':'코멘트 등록이 완료되었습니다!'})
 
-@app.route("/api/load_comment", methods=["GET"])
+# GET
+@app.route("/api/comments", methods=["GET"])
 def homework_get():
   comments_list = list(db.guest.find({},{'_id':False}))
   return jsonify({'comments_list': comments_list})
+
 
 
 if __name__ == '__main__':
